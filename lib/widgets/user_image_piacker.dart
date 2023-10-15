2169430961181a142dfca672c29409f2823cdd90
Osigbemhe Daniel <90054585+danielosi2@users.noreply.github.com>
2023-10-15 23:20:22 +0100

    Create user_image_piacker.dart

---
+++ lib/widgets/user_image_piacker.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserImagePicker extends StatefulWidget {
+  const UserImagePicker({super.key});
+
+  @override
+  State<UserImagePicker> createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
