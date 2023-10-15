bffb816545dfebd3f008e4ff1dc40ec7c45fb6ae
Osigbemhe Daniel <90054585+danielosi2@users.noreply.github.com>
2023-10-15 22:14:34 +0100

    keep user login in snapshot

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final _firebase = FirebaseAuth.instance;
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -14,12 +17,38 @@ class _AuthScreenState extends State<AuthScreen> {
   var _entredEmail = '';
   var _entredPassword = '';
 
-  void _submit() {
+  void _submit() async {
     final isValid = _form.currentState!.validate();
-    if (isValid) {
-      _form.currentState!.save();
-      print(_entredEmail);
-      print(_entredPassword);
+    if (!isValid) {
+      return;
+    }
+
+    _form.currentState!.save();
+
+    try {
+      if (_isLogin) {
+        final userCredentials = await _firebase.signInWithEmailAndPassword(
+          email: _entredEmail,
+          password: _entredPassword,
+        );
+      } else {
+        final userCredentials = await _firebase.createUserWithEmailAndPassword(
+          email: _entredEmail,
+          password: _entredPassword,
+        );
+      }
+    } on FirebaseAuthException catch (error) {
+      // if (error.code == 'email-already-in-use') {
+      //   //...
+      // }
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            error.message ?? 'Autentication Failed',
+          ),
+        ),
+      );
     }
   }
 
